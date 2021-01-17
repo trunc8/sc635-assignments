357fc4d236b00fef5166e28329fac98b8f8a9a50
Siddharth Saha <sahasiddharth611@gmail.com>
2021-01-17 22:07:09 +0530

    ApproxTimeSynchronizer not a good way; replaced with a more robust logic

--- week1_170100025_16d070043/scripts/joint_subscriber.py
+++ week1_170100025_16d070043/scripts/joint_subscriber.py
@@ -2,23 +2,36 @@
 
 import rospy
 from std_msgs.msg import String
-import message_filters
 
-def callback(names_msg, rolls_msg):
-  names = names_msg.data
-  name1, name2 = names.split("_")
-  rolls = rolls_msg.data
-  roll1, roll2 = rolls.split("_")
-  print("Student %s has roll : %s \nStudent %s has roll : %s" % (name1, roll1, name2, roll2))
+class Team:
+  def __init__(self):
+    self.names = None
+    self.rolls = None
+
+  def namesCallback(self, msg):
+    self.names = msg.data
+    self.getMembers()
+
+  def rollsCallback(self, msg):
+    self.rolls = msg.data
+    self.getMembers()
+
+  def getMembers(self):
+    if self.names!=None and self.rolls!=None:
+      name1, name2 = self.names.split("_")
+      roll1, roll2 = self.rolls.split("_")
+      print("Student %s has roll : %s \nStudent %s has roll : %s" % (name1, roll1, name2, roll2))
 
 rospy.init_node("joint_subscriber_node")
 rospy.loginfo("Subscriber created")
 
-names_sub = message_filters.Subscriber("names_topic", String)
-rolls_sub = message_filters.Subscriber("rolls_topic", String)
-ts = message_filters.ApproximateTimeSynchronizer([names_sub, rolls_sub], queue_size=10, slop=0.1, allow_headerless=True)
-ts.registerCallback(callback)
-rospy.loginfo("Time synchronized subscriber object created")
+team = Team()
+
+while (rospy.get_time()==0): # Wait until node has loaded completely
+  pass
+
+rospy.Subscriber("names_topic", String, team.namesCallback)
+rospy.Subscriber("rolls_topic", String, team.rollsCallback)
 
 rospy.loginfo("Beginning subscription...")
 rospy.spin()
